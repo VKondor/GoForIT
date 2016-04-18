d66efe1d36c3ad2aaff6101cd4c4d67cca5d01f2
VKondor <kondor2507@gmail.com>
2016-04-18 18:12:24 +0300

    Added HW 7-8 JS

--- JS_Module_6-7/js/script.js
+++ JS_Module_6-7/js/script.js
@@ -11,5 +11,25 @@ $(function() {
 
     $text.removeAttr('id');
     $text.eq($index).attr('id', 'active-text');
+  });
+
+  var $field = $('.field');
+  var $title = $('.title');
+  var $help = $('.help');
+
+  $field.hover(function() {
+    var $indexSecond = $(this).index();
+    $title.eq($indexSecond).css('display', 'block');
+    $title.eq($indexSecond).animate({ opacity: 1.0 }, 200);
+  }, function() {
+    var $indexThird = $(this).index();
+    $title.eq($indexThird).animate({ opacity: 0.0 }, 400);
+    $title.eq($indexThird).css('display', 'none');
+  });
+
+  $help.on('click', function(e) {
+    $title.css('display', 'block');
+    $title.animate({ opacity: 1.0 }, 200);
   })
 })
+
